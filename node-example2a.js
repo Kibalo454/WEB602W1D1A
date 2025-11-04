ab259b032c4bbdb6a5c55b2acf6ae778a9e10054
Kibalo454 <k.passigue.9247@westcliff.edu>
2025-11-03 20:13:52 -0500

    web

--- node-example2a.js
+++ node-example2a.js
@@ -1,1 +1,3 @@
+cont circle=require('./node-example2.js')
+console.log('The area of a circle of radius 4 is ${circle.area(4)}');
 
--- node-example3.js
+++ node-example3.js
@@ -2,3 +2,17 @@ const rect = {
     perimeter: (x, y) => 2 * (x + y),
     area: (x, y) => x * y
 };
+function solveRect(l,w) {
+    console.log('Solving for rectangle with dimensions: ${1}, ${w}'); // L and W
+    if(1<=0 || w<=0) {
+        console.log('Rectangle dimensions should be greater than zero: ${1}, ${w}'); // Prevents rectangle values less than 1
+    } else {
+        console.log('Area of rectangle is ${rect.area(1,w)}'); // Area
+        console.log('perimeter of rectangle: $(rect.perimeter(1,w)}'); // Perimeter
+    }
+}
+
+solveRect(6,7);
+solveRect(8,1);
+solveRect(0,9);
+solveRect(4, -1);
